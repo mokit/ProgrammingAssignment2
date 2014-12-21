1de454238be9a02aeee0837914739ad2d9426beb
Hans Vanderstraeten <hans@wieni.be>
2014-12-21 13:41:09 +0100

    added my response

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+## we will run this with our matrix before starting
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  invmat <- NULL
+  set <- function(y) {
+    mat <<- y
+    ## this way we reset the inversed cached matrix when we load a new matrix
+    invmat <<- NULL
+  }
+  get <- function() mat
+  setsolve <- function(solve) invmat <<- solve
+  getsolve <- function() invmat
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+## above. If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
+## we are using our matrix, which is now in fact a list (after using it with makeCacheMatrix)
+## for example
+## mat <- matrix(1:4,2,2)
+## preppedmat <- makeCacheMatrix(mat) ## preppedmat is a list
+## result <- cacheSolve(preppedmat) ## this will check if we have a cached version or not
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  ## check if we have a cached version already, because this is set to NULL when setting a new matrix in the makeCacheMatrix
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  mat <- x$get()
+  print(typeof(mat))
+  m <- solve(mat)
+  x$setsolve(m)
+  m
 }
